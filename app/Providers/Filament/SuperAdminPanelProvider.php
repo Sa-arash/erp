1b7af52a7165c99900c6d890f096c771bd74cfd7
saman <138027864+Sa-arash@users.noreply.github.com>
2025-01-28 17:02:54 +0330

    Update SuperAdminPanelProvider.php

--- app/Providers/Filament/SuperAdminPanelProvider.php
+++ app/Providers/Filament/SuperAdminPanelProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers\Filament;
 
 use Filament\Http\Middleware\Authenticate;
+use BezhanSalleh\FilamentShield\FilamentShieldPlugin;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
 use Filament\Pages;
@@ -52,6 +53,9 @@ class SuperAdminPanelProvider extends PanelProvider
                 SubstituteBindings::class,
                 DisableBladeIconComponents::class,
                 DispatchServingFilamentEvent::class,
+            ])
+            ->plugins([
+                FilamentShieldPlugin::make(),
             ])
             ->authMiddleware([
                 Authenticate::class,
