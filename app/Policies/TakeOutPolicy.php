3336e552fcfb4b5069d18323d35353a341b15fd4
saman <138027864+Sa-arash@users.noreply.github.com>
2025-01-29 08:31:51 +0330

    addedplicy

---
+++ app/Policies/TakeOutPolicy.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use App\Models\TakeOut;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class TakeOutPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return $user->can('view_any_take::out');
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, TakeOut $takeOut): bool
+    {
+        return $user->can('view_take::out');
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return $user->can('create_take::out');
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, TakeOut $takeOut): bool
+    {
+        return $user->can('update_take::out');
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, TakeOut $takeOut): bool
+    {
+        return $user->can('delete_take::out');
+    }
+
+    /**
+     * Determine whether the user can bulk delete.
+     */
+    public function deleteAny(User $user): bool
+    {
+        return $user->can('delete_any_take::out');
+    }
+
+    /**
+     * Determine whether the user can permanently delete.
+     */
+    public function forceDelete(User $user, TakeOut $takeOut): bool
+    {
+        return $user->can('force_delete_take::out');
+    }
+
+    /**
+     * Determine whether the user can permanently bulk delete.
+     */
+    public function forceDeleteAny(User $user): bool
+    {
+        return $user->can('force_delete_any_take::out');
+    }
+
+    /**
+     * Determine whether the user can restore.
+     */
+    public function restore(User $user, TakeOut $takeOut): bool
+    {
+        return $user->can('restore_take::out');
+    }
+
+    /**
+     * Determine whether the user can bulk restore.
+     */
+    public function restoreAny(User $user): bool
+    {
+        return $user->can('restore_any_take::out');
+    }
+
+    /**
+     * Determine whether the user can replicate.
+     */
+    public function replicate(User $user, TakeOut $takeOut): bool
+    {
+        return $user->can('replicate_take::out');
+    }
+
+    /**
+     * Determine whether the user can reorder.
+     */
+    public function reorder(User $user): bool
+    {
+        return $user->can('reorder_take::out');
+    }
+}
