fec702828332fdf3d89947e505c0bfbbab9275ce
saman <138027864+Sa-arash@users.noreply.github.com>
2025-02-01 08:42:12 +0330

    fix

--- app/Filament/Admin/Resources/PurchaseOrderResource/Pages/ListPurchaseOrders.php
+++ app/Filament/Admin/Resources/PurchaseOrderResource/Pages/ListPurchaseOrders.php
@@ -6,22 +6,35 @@ use App\Filament\Admin\Resources\FinancialPeriodResource;
 use App\Filament\Admin\Resources\PurchaseOrderResource;
 use App\Models\PurchaseOrder;
 use Filament\Actions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\ListRecords;
 
 class ListPurchaseOrders extends ListRecords
 {
     protected static string $resource = PurchaseOrderResource::class;
 
-    protected function getHeaderActions(): array
+    public function mount(): void
     {
         if (getPeriod()) {
+        Notification::make('financialErors')->seconds(5)->danger()->title('There is no financial period.')->send()->sendToDatabase(auth()->user());
+        }
+        $this->authorizeAccess();
+
+        $this->loadDefaultActiveTab();
+    }
+    protected function getHeaderActions(): array
+    {
+        if (!getPeriod()) {
 
             return [
                 Actions\CreateAction::make()
                     ->url(getPeriod() ? PurchaseOrderResource::getUrl('create') : FinancialPeriodResource::getUrl('index'))->label('New Purchase Order '),
             ];
         }else{
-            return [];
+            return [
+
+                Actions\Action::make('financialEror')->label('There is no financial period. Click to create')->url(fn()=>FinancialPeriodResource::getUrl('index'))->color('danger')
+            ];
         }
     }
 }
