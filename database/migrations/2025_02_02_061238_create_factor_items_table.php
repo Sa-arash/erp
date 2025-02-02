105e200faf90ae66753b98deb7a4eb32c3aa8f2b
SAJDEV <125678555+SAJDEV-s@users.noreply.github.com>
2025-02-02 09:52:07 +0330

    add factor and factor model

---
+++ database/migrations/2025_02_02_061238_create_factor_items_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('factor_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('factor_id')->constrained('factors')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->string('title');
+            $table->integer('quantity');
+            $table->foreignId('unit_id')->constrained('units')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->bigInteger('unit_price');
+            $table->integer('discount')->default(0);
+            $table->bigInteger('total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('factor_items');
+    }
+};
