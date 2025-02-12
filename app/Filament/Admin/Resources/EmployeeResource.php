c0de9396117f5d2c67c4a355e6343ac42e36c999
SAJDEV <125678555+SAJDEV-s@users.noreply.github.com>
2025-02-12 11:34:48 +0330

    Update EmployeeResource.php

--- app/Filament/Admin/Resources/EmployeeResource.php
+++ app/Filament/Admin/Resources/EmployeeResource.php
@@ -528,7 +528,7 @@ class EmployeeResource extends Resource implements HasShieldPermissions
                 Tables\Actions\Action::make('pdf')->tooltip('Print')->icon('heroicon-s-printer')->iconSize(IconSize::Medium)->label('')
                     ->url(fn($record) => route('pdf.employee', ['id' => $record->id]))->openUrlInNewTab(),
                 Tables\Actions\ActionGroup::make([
-                    Tables\Actions\Action::make('makeUser')->icon('heroicon-o-user-circle')->label('Make User')->form([
+                    Tables\Actions\Action::make('makeUser')->icon('heroicon-o-user-circle')->label('Create User')->form([
                         Forms\Components\Section::make('')->schema([
                             Forms\Components\Select::make('roles')->model(User::class)
                                 ->options(Role::query()->where('company_id',getCompany()->id)->pluck('name','id'))
