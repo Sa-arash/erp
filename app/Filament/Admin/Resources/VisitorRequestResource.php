4d48cea5e0d762395194a927797fd856fa4ed9c0
SAJDEV <125678555+SAJDEV-s@users.noreply.github.com>
2025-02-12 10:07:50 +0330

    fix

--- app/Filament/Admin/Resources/VisitorRequestResource.php
+++ app/Filament/Admin/Resources/VisitorRequestResource.php
@@ -5,6 +5,7 @@ namespace App\Filament\Admin\Resources;
 use App\Filament\Admin\Resources\VisitorRequestResource\Pages;
 use App\Filament\Admin\Resources\VisitorRequestResource\RelationManagers;
 use App\Models\VisitorRequest;
+use BezhanSalleh\FilamentShield\Contracts\HasShieldPermissions;
 use Filament\Forms;
 use Filament\Forms\Components\Section;
 use Filament\Forms\Form;
@@ -17,13 +18,26 @@ use Filament\Tables;
 use Filament\Tables\Table;
 use Malzariey\FilamentDaterangepickerFilter\Filters\DateRangeFilter;
 
-class VisitorRequestResource extends Resource
+class VisitorRequestResource extends Resource implements HasShieldPermissions
 {
     protected static ?string $model = VisitorRequest::class;
     protected static ?string $navigationLabel = 'Visit Access Request';
     protected static ?string $navigationGroup = 'Security Management';
     protected static ?int $navigationSort = 100;
     protected static ?string $navigationIcon = 'heroicon-o-eye';
+
+    public static function getPermissionPrefixes(): array
+    {
+        return [
+            'view',
+            'view_any',
+            'create',
+            'update',
+            'delete',
+            'delete_any',
+            'reception',
+        ];
+    }
 
     public static function form(Form $form): Form
     {
@@ -37,53 +51,53 @@ class VisitorRequestResource extends Resource
                             ->required()
                             ->options(getCompany()->employees->pluck('fullName', 'id'))
                             ->default(fn() => auth()->user()->employee->id),
-                            Forms\Components\DatePicker::make('visit_date')->default(now()->addDay())->required(),
+                        Forms\Components\DatePicker::make('visit_date')->default(now()->addDay())->required(),
                         Forms\Components\TimePicker::make('arrival_time')
-                        ->seconds(false)
-                        ->before('departure_time')
+                            ->seconds(false)
+                            ->before('departure_time')
                             ->required(),
                         Forms\Components\TimePicker::make('departure_time')
-                        ->seconds(false)
-                        ->after('arrival_time')
+                            ->seconds(false)
+                            ->after('arrival_time')
                             ->required(),
                         Forms\Components\TextInput::make('purpose')->columnSpanFull()
-                        ->required(),
-                        ])->columns(4),
+                            ->required(),
+                    ])->columns(4),
                     Forms\Components\Repeater::make('visitors_detail')
-                    ->addActionLabel('Add')
-                    ->label('Visitors Detail')
-                    ->schema([
-                        Forms\Components\TextInput::make('name')
-                        ->label('Full Name')
-                            ->required(),
-                        Forms\Components\TextInput::make('id')
-                            ->label('ID/Passport')
-                            ->required(),
-                        Forms\Components\TextInput::make('phone')
-                            ->label('Phone'),
-                        Forms\Components\TextInput::make('organization')
-                            ->label('Organization'),
-                        Forms\Components\Select::make('type')
-                            ->label('Type')
-                            ->options([
-                                'National' => 'National',
-                                'International' => 'International',
-                                'De-facto Security Forces' => 'De-facto Security Forces',
-                            ]),
-                        Forms\Components\TextInput::make('remarks')
-                            ->label('Remarks'),
+                        ->addActionLabel('Add')
+                        ->label('Visitors Detail')
+                        ->schema([
+                            Forms\Components\TextInput::make('name')
+                                ->label('Full Name')
+                                ->required(),
+                            Forms\Components\TextInput::make('id')
+                                ->label('ID/Passport')
+                                ->required(),
+                            Forms\Components\TextInput::make('phone')
+                                ->label('Phone'),
+                            Forms\Components\TextInput::make('organization')
+                                ->label('Organization'),
+                            Forms\Components\Select::make('type')
+                                ->label('Type')
+                                ->options([
+                                    'National' => 'National',
+                                    'International' => 'International',
+                                    'De-facto Security Forces' => 'De-facto Security Forces',
+                                ]),
+                            Forms\Components\TextInput::make('remarks')
+                                ->label('Remarks'),
 
-                    ])->columns(6)->columnSpanFull(),
+                        ])->columns(6)->columnSpanFull(),
                     Forms\Components\Repeater::make('driver_vehicle_detail')
-                    ->addActionLabel('Add')
-                    ->label('Drivers/Vehicles Detail')->schema([
-                        Forms\Components\TextInput::make('name')->label('Full Name')->required(),
-                        Forms\Components\TextInput::make('id')->label('ID/Passport')->required(),
-                        Forms\Components\TextInput::make('phone')->label('Phone'),
+                        ->addActionLabel('Add')
+                        ->label('Drivers/Vehicles Detail')->schema([
+                            Forms\Components\TextInput::make('name')->label('Full Name')->required(),
+                            Forms\Components\TextInput::make('id')->label('ID/Passport')->required(),
+                            Forms\Components\TextInput::make('phone')->label('Phone'),
                             Forms\Components\TextInput::make('model')->required(),
-                        Forms\Components\TextInput::make('color')->required(),
-                        Forms\Components\TextInput::make('Registration_Plate')->required(),
-                    ])->columns(6)->columnSpanFull(),
+                            Forms\Components\TextInput::make('color')->required(),
+                            Forms\Components\TextInput::make('Registration_Plate')->required(),
+                        ])->columns(6)->columnSpanFull(),
                     Forms\Components\Hidden::make('company_id')
                         ->default(getCompany()->id)
                         ->required(),
@@ -93,12 +107,12 @@ class VisitorRequestResource extends Resource
 
     public static function table(Table $table): Table
     {
-        return $table->defaultSort('visit_date','desc')
+        return $table->defaultSort('visit_date', 'desc')
             ->columns([
 
                 Tables\Columns\TextColumn::make('')->rowIndex(),
                 Tables\Columns\TextColumn::make('employee.fullName')
-                    ->label('Requestor')
+                    ->label('Requester')
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('visitors_detail')
@@ -109,8 +123,8 @@ class VisitorRequestResource extends Resource
                 Tables\Columns\TextColumn::make('visit_date')->date()->sortable(),
                 Tables\Columns\TextColumn::make('arrival_time')->time('H:m'),
                 Tables\Columns\TextColumn::make('departure_time')->time('H:m'),
-                Tables\Columns\TextColumn::make('status')->color(function($state){
-                    switch ($state){
+                Tables\Columns\TextColumn::make('status')->color(function ($state) {
+                    switch ($state) {
                         case "approved":
                             return 'success';
                         case "Pending":
@@ -123,38 +137,49 @@ class VisitorRequestResource extends Resource
                 Tables\Columns\TextColumn::make('created_at')->dateTime()->sortable()->toggleable(isToggledHiddenByDefault: true),
             ])
             ->filters([
-                Tables\Filters\SelectFilter::make('employee_id')->options(getCompany()->employees->pluck('info','id'))->searchable()->preload()->label('Employee'),
+                Tables\Filters\SelectFilter::make('employee_id')->options(getCompany()->employees->pluck('info', 'id'))->searchable()->preload()->label('Employee'),
                 DateRangeFilter::make('visit_date')->label('Visit Date'),
-                Tables\Filters\SelectFilter::make('status')->options(['approved'=>'approved','notApproved'=>'notApproved'])->searchable()
-            ],getModelFilter())
+                Tables\Filters\SelectFilter::make('status')->options(['approved' => 'approved', 'notApproved' => 'notApproved'])->searchable()
+            ], getModelFilter())
             ->actions([
                 Tables\Actions\Action::make('ActionInSide')->label('CheckIn')->form([
                     Forms\Components\DateTimePicker::make('InSide_date')->withoutSeconds()->label(' Date And Time')->required()->default(now()),
                     Forms\Components\Textarea::make('inSide_comment')->label(' Comment')
                 ])->requiresConfirmation()->action(function ($data, $record) {
-                    $record->update(['InSide_date' => $data['InSide_date'], 'inSide_comment' => $data['inSide_comment'],'gate_status'=>'CheckedIn']);
+                    $record->update(['InSide_date' => $data['InSide_date'], 'inSide_comment' => $data['inSide_comment'], 'gate_status' => 'CheckedIn']);
                     Notification::make('success')->success()->title('Submitted Successfully')->send();
-                })->hidden(fn($record)=>$record->InSide_date or $record->status==="notApproved"),
+                })->visible(function ($record) {
+                    if (auth()->user()->can('reception_visitor::request')) {
+                        if ($record->status === "approved") {
+                            if ($record->InSide_date === null) {
+                                return true;
+                            }
+                        }
+                    }
+                    return false;
+                }),
                 Tables\Actions\Action::make('ActionOutSide')->label('CheckOut')->form([
                     Forms\Components\DateTimePicker::make('OutSide_date')->withoutSeconds()->label(' Date And Time')->required()->default(now()),
                     Forms\Components\Textarea::make('OutSide_comment')->label(' Comment')
                 ])->requiresConfirmation()->action(function ($data, $record) {
-                    $record->update(['OutSide_date' => $data['OutSide_date'], 'OutSide_comment' => $data['OutSide_comment'],'gate_status'=>'CheckedOut']);
+                    $record->update(['OutSide_date' => $data['OutSide_date'], 'OutSide_comment' => $data['OutSide_comment'], 'gate_status' => 'CheckedOut']);
                     Notification::make('success')->success()->title('Submitted Successfully')->send();
-                })->visible(function($record){
-                    if ($record->OutSide_date!==null ){
-                        return false;
+                })->visible(function ($record) {
+                    if (auth()->user()->can('reception_visitor::request')) {
+                        if ($record->OutSide_date !== null) {
+                            return false;
+                        }
+                        if ($record->InSide_date !== null) {
+                            return true;
+                        }
                     }
-                    if ($record->InSide_date !==null ){
-                        return true;
-                    }
-                    return  false;
+                    return false;
                 }),
 
 
                 Tables\Actions\ViewAction::make()->infolist([
                     \Filament\Infolists\Components\Section::make([
-                       TextEntry::make('employee.info')->label('Employee'),
+                        TextEntry::make('employee.info')->label('Employee'),
                         RepeatableEntry::make('visitors_detail')->schema([
                             TextEntry::make('name'),
                             TextEntry::make('id')->label('ID/Passport'),
