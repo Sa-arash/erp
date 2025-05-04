879ccbd264311ec37ff2dd1e37c49edcf1d8cfea
saman <138027864+Sa-arash@users.noreply.github.com>
2025-05-04 22:24:36 +0330

    Update myEmployees.php

--- app/Filament/Admin/Pages/myEmployees.php
+++ app/Filament/Admin/Pages/myEmployees.php
@@ -8,13 +8,13 @@ use Filament\Pages\Page;
 class myEmployees extends Page
 {
     protected static ?string $navigationIcon = 'heroicon-o-user-group';
-    use HasPageShield;
+
 
     protected static string $view = 'filament.admin.pages.my-employees';
 
     protected static ?string $navigationLabel='My Employees';
-    protected function canView(): bool
-    {
+   public static function canAccess(): bool
+{
     
         return !(auth()->user()->employee->subordinates->isEmpty());
     }
